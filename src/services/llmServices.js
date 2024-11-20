91682f1356338868d397b4d486a9ed2f33edfa69
Achsaf Atzmon <achsaf6@gmail.com>
2024-11-20 22:10:54 +0200

    refactoring

--- src/services/llmServices.js
+++ src/services/llmServices.js
@@ -1,8 +1,8 @@
-// import axios from "axios";
 
 import OpenAI from "openai"; 
 
-const API_KEY = "" 
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const client = new OpenAI({
     apiKey: API_KEY, 
